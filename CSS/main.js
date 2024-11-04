ae7ed60479a62209a22f01ac7d53b41cf0f559f1
Kayzendrew <140522846+Kayzendrew@users.noreply.github.com>
2024-11-04 11:42:45 -0300

    04/11/2024 - aula de programação
    Adicionamos  detalhes  na  função criada em Javascript. Ligamos uma class CSS a ela e desenvolvemos outras com mesma  estilização.

--- CSS/main.js
+++ CSS/main.js
@@ -1,4 +1,20 @@
-function criarCartao(tema,pergunta,resposta){
-
-    
+function criarCartao(tema, pergunta, resposta) {
+    const container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+        <div class="conteudo-cartao">
+        <h3> ${tema}</h3>
+             <div class="cartao-conteudo-pergunta">
+             ${pergunta}
+            </div>
+            <div class="cartao-conteudo-resposta">
+            ${resposta}
+            </div>
+        </div>
+        `
+    container.appendChild(cartao);
 }
+criarCartao('história', 'Em que ano iniciou-se a guerra do Vietnã?', 'Em 1955');
+
+criarCartao('Portugues', `O que é uma proparoxitona?`, `Uma classificação gramatical.`)
